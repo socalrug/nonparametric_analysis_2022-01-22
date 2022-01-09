d75af10f7053386cb21ab105f2c37f420fb5a8ea
John Peach <githubspam@jppeach.com>
2022-01-08 22:59:17 -0800

    first have materials

---
+++ examples/Example3.1.R
@@ -0,0 +1,3 @@
+Tx<- c(3, 4, 6, 6, 8, 8)
+Cx<- c(4, 4, 4, 5, 6, 7, 8, 10, 12)
+ks.test(Tx,Cx, alternative="greater", exact=FALSE)
--- exercises/Exercise6.1Solution.R
+++ exercises/Exercise6.1Solution.R
@@ -1,4 +1,5 @@
 center<- c('Stockholm','Washington','Denver')
 age<- c('20s30s','40s50s','60s','70splus')
-data<- matrix(c(5, 10, 13, 12, 8, 11, 4, 3, 2, 14, 8, 0), nrow = 3, dimnames = list(center, age))
+data<- matrix(c(5, 10, 13, 12, 8, 11, 4, 3, 2, 14, 8, 0), 
+nrow = 3, dimnames = list(center, age))
 fisher.test(data)
